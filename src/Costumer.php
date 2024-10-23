ea317b41ff4887a66a95fee1f239f7cd16b7acb7
Mathew Geraldo Pedro Rottye <mathewgeraldo69@gmail.com>
2024-10-23 10:29:01 +0700

    Contoh library sederhana Composer

---
+++ src/Costumer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mager\Belajar;
+
+class Costumer
+{
+    public function __construct(private string $name) {}
+
+    public function sayHello(string $name): string
+    {
+        return "Hello {$name}, my name is $this->name";
+    }
+}
